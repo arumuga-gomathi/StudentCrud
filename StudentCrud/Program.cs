6d0628d2a8c97c9c9cff6a2c7ffbed879724ac3f
ARUMUGA GOMATHI <r.arumugagomathi@gmail.com>
2025-06-03 09:04:15 +0530

    initial commit

--- StudentCrud/Program.cs
+++ StudentCrud/Program.cs
@@ -15,20 +15,17 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
+// Default route to StudentsController
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Students}/{action=Index}/{id?}");
-
 
 app.Run();
